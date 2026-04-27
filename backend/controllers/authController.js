6f64b81ce4f465523982b858b575ac8dd42ab722
Vansh Nagpal <nagpalv931@gmail.com>
2026-04-27 23:53:57 +0530

    adding data modelling files

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,3 +8,47 @@ const generateToken = (id) => {
     });
 }
 
+// @desc Register new user
+// @route POST /api/auth/register
+// @access Public
+
+export const register = async (req,res,next) => {
+    try{
+
+    } catch(error){
+        next(error);
+    }
+}
+
+// @desc Login user
+// @route POST /api/auth/login
+// @access Public
+
+export const login = async (req,res,next) => {
+
+};
+
+// @desc Get user profile
+// @route GET /api/auth/profile
+// @access Private
+
+export const getProfile = async (req,res,next) => {
+
+}
+
+
+// @desc Update user profile
+// @route PUT /api/auth/profile
+// @access Private
+
+export const updateProfile = async (req,res,next) => {
+
+};
+
+// @desc Change password
+// @route POST /api/auth/change-password
+// @access Private
+
+export const changePassword = async (req,res,next) => {
+
+}
